e11354a5cdd5e84982e9c5ca3df30591979a5318
phenotypic <33377034+phenotypic@users.noreply.github.com>
2021-12-24 23:38:32 +0000

    Rename

--- examples/NodeMCU-Switch.ino
+++ examples/NodeMCU-Switch.ino
@@ -1,7 +1,7 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266mDNS.h>
 
-// GitHub Page = https://github.com/Tommrodrigues/homebridge-web-switch
+// GitHub Page = https://github.com/phenotypic/homebridge-web-switch
 
 // Script Type = Switch relay
 
